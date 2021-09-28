d9293cc58becaed67bbda927849bac558a8b5978
ywgx <ywgx@xabcloud.com>
2021-09-28 13:50:19 +0800

    update

--- xabc_k8s.sh
+++ xabc_k8s.sh
@@ -1,10 +1,7 @@
 export EDITOR=vim
 export KUBE_EDITOR=vim
-source <(kubectl completion bash)
 alias vi=vim
 alias ls='ls --color'
 alias ll='ls -l'
-alias ks='/usr/local/bin/ki.py -s'
-alias ki='/usr/local/bin/ki.py -n'
 export PS1="\e[1;37m[\e[m\e[1;36m\u\e[m\e[1;36m@\e[m\e[1;36m\H\e[m \e[1;33m\A\e[m \w\e[m\e[1;37m]\e[m\e[1;36m\e[m\n\\$ "
 [[ $- == *i* ]]&&printf "\033c\033[5;32;40m%s\033[1;m\n" "[ $(ls -l ~/.kube/config|awk -F'/' '{print $NF}') ]"

62f4ed43bbaf07587fe955eaf257c4c51a36244f
ywgx <ywgx@xabcloud.com>
2023-04-27 16:33:24 +0800

    update

--- zki.sh
+++ zki.sh
@@ -1,8 +1,11 @@
+function parse_git_branch() {
+  [ -e ./.git/HEAD ] && echo -e "* \033[32m$(awk -F/ '{print $NF}' ./.git/HEAD)\033[0m"
+}
 source <(kubectl completion bash)
 export EDITOR=vim
 export KUBE_EDITOR=vim
 export KI_LINE=$([ -e ~/.history/.line ] && cat ~/.history/.line || echo 200)
-export PS1="\e[1;37m[\e[m\e[1;32m\u\e[m\e[1;32m@\e[m\e[1;35m\H\e[m \e[1;33m\A\e[m \w\e[m\e[1;37m]\e[m\e[1;36m\e[m \$(/usr/local/bin/ki --w) \n\\$ "
+export PS1="\e[1;37m[\e[m\e[1;32m\u\e[m\e[1;32m@\e[m\e[1;35m\H\e[m \e[1;33m\A\e[m \w\e[m\e[1;37m]\e[m\e[1;36m\e[m \$(/usr/local/bin/ki --w) \$(parse_git_branch) \n\\$ "
 alias vi=vim
 alias ls='ls --color'
 alias ll='ls -l'

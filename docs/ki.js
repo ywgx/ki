34e584937662955c873ee8fb51df662a277eaa83
ywgx <ywgx@xabcloud.com>
2023-06-06 11:09:47 +0800

    2023-06-06-11:09:47

---
+++ docs/ki.js
@@ -0,0 +1,18 @@
+window.$docsify = {
+        auto2top:true,
+        coverpage:true,
+        executeScript: true,
+        loadSidebar:true,
+        maxLevel: 4,
+        subMaxLevel: 3,
+        themeColor:'#333333',
+        name:'𝒌𝒊',
+        search:{maxAge: 86400000, paths: 'auto', placeholder: '🔍', noData: '无信息'},
+        plugins:[ function (hook) {
+                        var footer = [
+                                '<footer>','<hr><small> © Ki Authors 2021-2023 | Documentation Distributed under CC-BY-4.0</small>','</footer>'
+                        ].join('')
+                        hook.afterEach(function (html){return html+footer})
+                }
+        ]
+}

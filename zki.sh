50cd3f7040fe11bb0add8653ec688f0492eb2522
ywgx <ywgx@xabcloud.com>
2023-10-24 17:21:47 +0800

    2023-10-24-17:21:47

--- zki.sh
+++ zki.sh
@@ -11,5 +11,6 @@ export PS1="\e[1;37m[\e[m\e[1;32m\u\e[m\e[1;32m@\e[m\e[1;35m\H\e[m \e[1;33m\A\e[
 alias vi=vim
 alias ls='ls --color'
 alias ll='ls -l'
+alias kn='cd ~/.kube;for file in $(find . -name "kubeconfig-*-NULL"); do mv "$file" "${file%-NULL}"; done'
 [ $USER = root ] && STYLE="\033c\033[5;32m%s\033[1;m\n" || STYLE="\033[5;32m%s\033[1;m\n"
 [[ $- == *i* ]] && [ -e ~/.kube/config ] && printf $STYLE "$(/usr/local/bin/ki --w)"
